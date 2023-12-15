be670ce14233db7a7d62b6f6b75271739a3c50e8
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2023-12-15 14:21:30 +0300

    Create project

---
+++ iCalories/iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Ваня Науменко on 15.12.23.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
