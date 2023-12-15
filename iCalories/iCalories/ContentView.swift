c79d218f4c16716bd8103475d520977948b0129a
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2023-12-15 14:22:32 +0300

    Text new

--- iCalories/iCalories/ContentView.swift
+++ iCalories/iCalories/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Go GO GO GO")
         }
         .padding()
     }
